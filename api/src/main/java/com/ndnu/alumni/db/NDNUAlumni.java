618b2a81a9d484b41764724dc7b0f3eeed564eaa
James Whiteman <jameswhiteman@outlook.com>
2015-02-28 11:16:27 -0800

    db connection

---
+++ api/src/main/java/com/ndnu/alumni/db/NDNUAlumni.java
@@ -0,0 +1,40 @@
+package com.ndnu.alumni.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class NDNUAlumni
+{
+    private String host = "jdbc:mysql://localhost:3306";
+    private String user = "root";
+    private String password = "american";
+
+    private Connection connection;
+
+    public NDNUAlumni() throws SQLException
+    {
+        String url = host + "?user=" + user + "&password=" + password;
+        Connection connection = DriverManager.getConnection(this.host);
+    }
+
+    public List<Project> getProjects()
+    {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = null;
+        if (statement.execute("select * from "))
+        {
+            resultSet = statement.getResultSet();
+        }
+        if (resultSet == null)
+        {
+            return null;
+        }
+        while (resultSet.next())
+        {
+
+        }
+    }
+}
