4b781287a6640b254587fb66d83a1d19958a7d7e
James Whiteman <jameswhiteman@outlook.com>
2015-02-27 20:10:38 -0800

    updated frontend

---
+++ site/gulpfile.js
@@ -0,0 +1,26 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+
+// start server
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./app"
+        }
+    });
+});
+
+gulp.task('html', function () {
+    return gulp.src('./app/*.html')
+        .pipe(browserify())
+        .pipe(uglify())
+        .pipe(gulp.dest('app/'));
+});
+
+// use default task to launch BrowserSync and watch JS files
+gulp.task('default', ['browser-sync'], function () {
+
+    // add browserSync.reload to the tasks array to make
+    // all browsers reload after tasks are complete.
+    gulp.watch("./app/*.html", ['html', browserSync.reload]);
+});
