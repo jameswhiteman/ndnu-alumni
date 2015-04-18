7003853b64b3b8e899175260faca85d32b09eb10
engChaos <engchaos.ingress@gmail.com>
2015-04-17 19:57:07 -0700

    add login toast

--- site/app/js/loginCtrl.js
+++ site/app/js/loginCtrl.js
@@ -1,10 +1,41 @@
 var app = angular.module('app');
 
-app.controller('loginCtrl', ['$scope', '$http', '$state', function($scope, $http, $state)
+app.controller('loginCtrl', ['$scope', '$http', '$state', '$mdToast', '$animate', '$mdDialog', function($scope, $http, $state,$mdToast, $animate,$mdDialog)
 {
     $scope.data = [];
     $scope.email = "";
     $scope.password = "";
+
+  $scope.toastPosition = {
+    bottom: false,
+    top: true,
+    left: false,
+    right: true
+  };
+  $scope.getToastPosition = function() {
+    return Object.keys($scope.toastPosition)
+      .filter(function(pos) { return $scope.toastPosition[pos]; })
+      .join(' ');
+  };
+  $scope.showToast = function() {
+    $mdToast.show({
+      templateUrl: 'temp/userToast.html',
+      hideDelay: 6000,
+      position: $scope.getToastPosition()
+    })};
+
+     $scope.closeToast = function() {
+    $mdToast.hide();
+  };
+
+  function DialogController($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  }};
+
     $scope.submit = function() {
         var info = "?email="+this.email+"&password="+this.password;
         $http.get('http://localhost:8282/ndnualumni-api/users' + info).
@@ -16,6 +47,8 @@ app.controller('loginCtrl', ['$scope', '$http', '$state', function($scope, $http
             }
             else
             {
+                $scope.showToast();
+               $scope.hide();
                 $state.go('home')
             }
           }).
@@ -24,3 +57,4 @@ app.controller('loginCtrl', ['$scope', '$http', '$state', function($scope, $http
           });
     }
 }]);
+
